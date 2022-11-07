acc3764379bd154e27330739b4e2008e3d1375ba
Swayamshree <mohantyswayamshree@gmail.com>
2022-11-07 23:40:20 +0530

    geo locator plugin added

--- lib/location_service/repository/location_service_repository.dart
+++ lib/location_service/repository/location_service_repository.dart
@@ -2,7 +2,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:dio/dio.dart';
-import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:uuid/uuid.dart';
 import 'package:background_location_sender/location_service/key/.env.dart';
 import 'package:background_location_sender/location_service/model/location_address_with_latlong.dart';
@@ -12,10 +12,8 @@ import 'package:background_location_sender/location_service/model/location_by_ge
 
 class LocationServiceRepository {
   final dio = Dio();
-  Location location;
-  LocationServiceRepository({
-    required this.location,
-  });
+
+  LocationServiceRepository();
 
   Future<LocationAddressWithLatLong> fetchLocationByAddress({
     required String selectedAddress,
@@ -62,25 +60,41 @@ class LocationServiceRepository {
 
   Future<LocationAddressWithLatLong> fetchLocationByDeviceGPS() async {
     try {
-      bool serviceEnabled = await location.serviceEnabled();
-      if (!serviceEnabled) {
-        serviceEnabled = await location.requestService();
-        if (!serviceEnabled) {
-          throw ("Require GPS service enabled");
-        }
-      }
-      PermissionStatus permissionGranted = await location.hasPermission();
-      if (permissionGranted == PermissionStatus.denied) {
-        permissionGranted = await location.requestPermission();
-        if (permissionGranted != PermissionStatus.granted) {
-          throw ("Require GPS service permission");
-        }
-      }
+      bool serviceEnabled;
+      LocationPermission permission;
 
-      final locationData = await location.getLocation();
-      return await fetchLocationByCoOrdinates(
-          latitude: locationData.latitude ?? 17.6968072,
-          longitude: locationData.longitude ?? 83.2037749);
+      // Test if location services are enabled.
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        // Location services are not enabled don't continue
+        // accessing the position and request users of the
+        // App to enable the location services.
+        throw ('Location services are disabled.');
+      } else {
+        permission = await Geolocator.checkPermission();
+
+        if (permission == LocationPermission.denied) {
+          permission = await Geolocator.requestPermission();
+          if (permission == LocationPermission.denied) {
+            throw ('Location permissions are denied');
+          }
+        }
+        if (permission == LocationPermission.deniedForever) {
+          // Permissions are denied forever, handle appropriately.
+          throw ('Location permissions are permanently denied, we cannot request permissions.');
+        }
+        if (permission == LocationPermission.whileInUse) {
+          permission = await Geolocator.requestPermission();
+          // Permissions are denied forever, handle appropriately.
+          throw ('Set the location permissions to Always.');
+        }
+
+        final locationData = await Geolocator.getCurrentPosition();
+        return await fetchLocationByCoOrdinates(
+          latitude: locationData.latitude,
+          longitude: locationData.longitude,
+        );
+      }
     } catch (e) {
       rethrow;
     }
