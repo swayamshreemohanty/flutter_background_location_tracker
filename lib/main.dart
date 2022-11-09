1f8585bfac808b9820acdb8266e0722a5cd40cee
Swayamshree <mohantyswayamshree@gmail.com>
2022-11-09 19:02:20 +0530

    firebase notification methods added

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,71 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 final locationControllerCubit = LocationControllerCubit(
   locationServiceRepository: LocationServiceRepository(),
 );
 
-NotificationService notificationService = NotificationService();
+final notificationService =
+    NotificationService(FlutterLocalNotificationsPlugin());
 
-Future<void> backgroundHandler(RemoteMessage message) async {
-  await FirebaseMessaging.instance.requestPermission();
+/// Create a [AndroidNotificationChannel] for heads up notifications
+late AndroidNotificationChannel channel;
+
+Future<void> setupFlutterNotifications() async {
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  /// Create an Android Notification Channel.
+  ///
+  /// We use this channel in the `AndroidManifest.xml` file to override the
+  /// default FCM channel to enable heads up notifications.
+  await notificationService.createChannel(channel);
+
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+}
+
+void showFlutterNotification(RemoteMessage message) {
+  RemoteNotification? notification = message.notification;
+  AndroidNotification? android = message.notification?.android;
+  if (notification != null && android != null) {
+    notificationService.showNotification(
+      androidNotificationDetails: AndroidNotificationDetails(
+        channel.id,
+        channel.name,
+        channelDescription: channel.description,
+        // TODO add a proper drawable resource to android, for now using
+        //      one that already exists in example app.
+        // icon: 'launch_background',
+      ),
+      showNotificationId: notification.hashCode,
+      title: "Local test ${notification.title}",
+      body: "Local test ${notification.body}",
+      payload: "service",
+    );
+  }
+}
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await setupFlutterNotifications();
+  showFlutterNotification(message);
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
+  print('Handling a background message ${message.messageId}');
 }
 
 Future<void> main() async {
@@ -26,32 +80,39 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await notificationService.initialize();
 
-  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   // We don't need it anymore since it will be executed in background
 
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => UpdateLocationOnDbCubit(),
+    return RepositoryProvider.value(
+      value: notificationService,
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => UpdateLocationOnDbCubit(),
+          ),
+          BlocProvider.value(value: locationControllerCubit),
+        ],
+        child: MaterialApp(
+          title: 'Track Your Location',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomeScreen(),
         ),
-        BlocProvider.value(value: locationControllerCubit),
-      ],
-      child: MaterialApp(
-        title: 'Track Your Location',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomeScreen(),
       ),
     );
   }
